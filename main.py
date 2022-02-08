da6b463591d938dc456128241ee48410726a704a
Tarin <tarin.eccleston@gmail.com>
2022-02-08 16:36:41 +1300

    Issue with API, use open notify instead

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ import webbrowser
 import geocoder
 
 # get latitude, longitude, altitude and velocity information from API
-# url = "http://api.open-notify.org/iss-now.json"
-url = 'https://api.wheretheiss.at/v1/satellites/25544'
-response = urllib2.urlopen(url)
+url = "http://api.open-notify.org/iss-now.json"
+# url = "https://api.wheretheiss.at/v1/satellites/25544"
+response = urllib.request.urlopen(url)
 result = json.loads(response.read())
 file = open("iss.txt", "w")
-file.write("Latitude: " + result["latitude"] + " , Longitude: " + result["longitude"] +
-    " , Altitude: " + result["altitude"] + " , Velocity: " + result["velocity"])
-
-
+file.write("Latitude: " + result["iss_position"]["latitude"] + " , Longitude: " + result["iss_position"]["longitude"]) # +
+    # " , Altitude: " + result["altitude"] + " , Velocity: " + result["velocity"])
 
 

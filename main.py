e15b69cf6c79451223a060481d13ed12a325de60
Tarin <tarin.eccleston@gmail.com>
2022-02-08 18:29:06 +1300

    Working system

--- main.py
+++ main.py
@@ -5,26 +5,35 @@ import time
 import webbrowser
 import geocoder
 
-# get latitude, longitude from API
-url = "http://api.open-notify.org/iss-now.json"
-response = urllib.request.urlopen(url)
-result = json.loads(response.read())
-file = open("iss.txt", "w")
-file.write("Latitude: " + result["iss_position"]["latitude"] + " , Longitude: " + result["iss_position"]["longitude"])
-
-webbrowser.open("iss.txt")
-
-# setup screen for world map
+# setup screen for map
 screen = turtle.Screen()
-screen.setup(1280,720)
-screen.setworldcoordinates(-180,-90,180,90)
+screen.setup(1280, 720)
+screen.setworldcoordinates(-180, -90, 180, 90)
 
-# load image of world map
+# load the world map image
 screen.bgpic("map.gif")
 screen.register_shape("iss.gif")
 iss = turtle.Turtle()
 iss.shape("iss.gif")
 iss.setheading(45)
-iss.penup
+iss.penup()
 
-input("out")
+while True:
+
+    # get latitude, longitude from API
+    url = "http://api.open-notify.org/iss-now.json"
+    response = urllib.request.urlopen(url)
+    result = json.loads(response.read())
+
+    lat = result["iss_position"]["latitude"]
+    lon = result["iss_position"]["longitude"]
+
+    print("Latitude: " + lat + " , Longitude: " + lon)
+
+    lat = float(lat)
+    lon = float(lon)
+
+    iss.goto(lon, lat)
+
+    time.sleep(1)
+

0300d545bafa73388008b87073555cbf14f82ac7
Tarin <tarin.eccleston@gmail.com>
2022-02-08 22:31:38 +1300

    Fix Haversine formula, tidy up code, reject false measurements

--- main.py
+++ main.py
@@ -22,44 +22,53 @@ iss.penup()
 # initialise for Havresine formula
 lat_prev = 0.0
 lon_prev = 0.0
-# radius of earth
+initial = True
 R = 6371
+dt = 5
 
 while True:
-
     # get latitude, longitude from API
     url = "http://api.open-notify.org/iss-now.json"
     start = time.time()
     response = urllib.request.urlopen(url)
     result = json.loads(response.read())
 
-    # find and save lat and lon
+    # extract from data
+    timestamp = result["timestamp"]
     lat_current = result["iss_position"]["latitude"]
     lon_current = result["iss_position"]["longitude"]
+    print("Timestamp: " + str(timestamp))
     print("Latitude: " + lat_current + " , Longitude: " + lon_current)
+
+    # update iss map with new coordinates
     lat_current = float(lat_current)
     lon_current = float(lon_current)
-
-    time.sleep(1)
-    end = time.time()
-
     iss.goto(lon_current, lat_current)
 
-    dt = end - start
-    print(dt)
+    #  convert coordinates to radians
+    lon_current = lon_current * math.pi/180
+    lat_current = lat_current * math.pi/180
 
-    # use Havresine formala to find ISS velocity
-    dlong = lon_current - lon_prev;
-    dlat = lat_current - lat_prev;
-    
-    dtheta = 2 * math.asin(math.sqrt(pow(math.sin(dlat / 2), 2) + math.cos(lat_prev) * 
-        math.cos(lat_current) * pow(math.sin(dlong / 2), 2)))
+    # calculate server request delay to accept/reject measurements
+    end = time.time()
+    delay = end - start
 
-    ds = R * dtheta
+    if (~(lat_prev == 0.0) & ~(lon_prev == 0.0) & ((delay <= 1.0) | (delay >= 0.1))):
+        # use Haversine formala to find ISS velocity
+        dlong = lon_current - lon_prev;
+        dlat = lat_current - lat_prev;
         
-    v = ds/dt
-    print(v)
+        dtheta = 2 * math.asin(math.sqrt(pow(math.sin(dlat / 2), 2) + math.cos(lat_prev) * 
+            math.cos(lat_current) * pow(math.sin(dlong / 2), 2)))
+        ds = R * dtheta
+        
+        # differentiate to find velocity
+        v = ds/dt
+        v = round(v,4)
+        print("Velocity (km/s): " + str(v))
 
     lat_prev = lat_current
     lon_prev = lon_current
+    time.sleep(dt)
+
 

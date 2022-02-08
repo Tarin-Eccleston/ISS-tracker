85d01c4d28305679fc6ee63cb4603d26492c7eed
Tarin <tarin.eccleston@gmail.com>
2022-02-08 18:12:28 +1300

    Add map and iss gif and setup screen

--- main.py
+++ main.py
@@ -5,13 +5,26 @@ import time
 import webbrowser
 import geocoder
 
-# get latitude, longitude, altitude and velocity information from API
+# get latitude, longitude from API
 url = "http://api.open-notify.org/iss-now.json"
-# url = "https://api.wheretheiss.at/v1/satellites/25544"
 response = urllib.request.urlopen(url)
 result = json.loads(response.read())
 file = open("iss.txt", "w")
-file.write("Latitude: " + result["iss_position"]["latitude"] + " , Longitude: " + result["iss_position"]["longitude"]) # +
-    # " , Altitude: " + result["altitude"] + " , Velocity: " + result["velocity"])
+file.write("Latitude: " + result["iss_position"]["latitude"] + " , Longitude: " + result["iss_position"]["longitude"])
 
+webbrowser.open("iss.txt")
 
+# setup screen for world map
+screen = turtle.Screen()
+screen.setup(1280,720)
+screen.setworldcoordinates(-180,-90,180,90)
+
+# load image of world map
+screen.bgpic("map.gif")
+screen.register_shape("iss.gif")
+iss = turtle.Turtle()
+iss.shape("iss.gif")
+iss.setheading(45)
+iss.penup
+
+input("out")

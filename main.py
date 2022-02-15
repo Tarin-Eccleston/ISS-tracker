0853b7fb362132fddf6788e08715fc7efb1ca1ed
Tarin <tarin.eccleston@gmail.com>
2022-02-16 10:59:18 +1300

    Split TLE lines

--- main.py
+++ main.py
@@ -9,17 +9,12 @@ from skyfield.api import EarthSatellite
 # Make reques to ISS API for TLE data lines
 f = requests.get('https://api.wheretheiss.at/v1/satellites/25544/tles?format=text')
 output = f.text
-
-
-
-# print(output)
+output = output.split('\n', 3)
 
 # Extract TLE lines
-# name = output.split('\n', 1)[1]
-# line1 = output.split('\n', 2)[1]
-# line2 = output.split('\n', 3)[1]
-
-# print(name)
+name = output[0]
+line1 = output[1]
+line2 = output[2]
 
 # ts = skyfield.api.load.timescale()
 # satellite = EarthSatellite(line1, line2, name, ts)
